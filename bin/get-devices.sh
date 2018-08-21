b9fb13ba312255372321e18fb0ad1298e3e2842d
Chris Prather <chris@prather.org>
2018-08-21 02:37:41 +0000

    filter the devices we get in get-devices better

--- bin/get-devices.sh
+++ bin/get-devices.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-DEVICE_IDS=`conch -j ws GLOBAL devices | jq '.[] | select(.health | contains("PASS")) | .id' | sort -R | head -n 10 | tr -d '"'`
+DEVICE_IDS=`conch -j ws GLOBAL devices | jq 'sort_by(.created) | reverse | limit(500; .[]) | select(.health | contains("PASS")) | .id' | sort -R | head -n 10 | tr -d '"'`
 
 for DEVICE in $DEVICE_IDS
 do
